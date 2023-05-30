15154f2c515019ba87aaa706184530b961082e14
delay100 <qor3127@naver.com>
2023-05-30 15:56:15 +0900

    FEAT: start.spring.io 프로젝트

---
+++ src/main/java/com/dbdbdip/giftmanagement/GiftmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.dbdbdip.giftmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbdbdip/giftmanagement/GiftmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbdbdip.giftmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
