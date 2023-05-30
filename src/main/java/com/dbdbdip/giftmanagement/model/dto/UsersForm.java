f369349292d7eea7e46700e103ce4514c37d6ad1
sseen2 <59137639+sseen2@users.noreply.github.com>
2023-05-30 22:21:41 +0900

    FEAT: Users 기능 추가 및 MainController 추가

---
+++ src/main/java/com/dbdbdip/giftmanagement/model/dto/UsersForm.java
@@ -0,0 +1,40 @@
+package com.dbdbdip.giftmanagement.model.dto;
+
+public class UsersForm {
+    private String id;
+    private String password;
+    private String nickname;
+    private String userRole;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+}
