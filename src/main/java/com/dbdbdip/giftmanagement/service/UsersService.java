b3e0107f0d4ba4c6b7ad7a12554bfb9f65ef2313
sseen2 <59137639+sseen2@users.noreply.github.com>
2023-06-02 00:23:12 +0900

    FEAT: 로그인 기능 추가

--- src/main/java/com/dbdbdip/giftmanagement/service/UsersService.java
+++ src/main/java/com/dbdbdip/giftmanagement/service/UsersService.java
@@ -3,10 +3,9 @@ package com.dbdbdip.giftmanagement.service;
 import com.dbdbdip.giftmanagement.model.dto.UsersForm;
 import com.dbdbdip.giftmanagement.model.entity.Users;
 import com.dbdbdip.giftmanagement.repository.UsersRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +21,28 @@ public class UsersService {
                 .build();
 
         usersRepository.save(u);
+    }
+
+    public boolean login(UsersForm usersForm, HttpSession httpSession) {
+        System.out.println("******** 로그인 서비스 ********");
+
+        if (usersForm.getUserId().length() == 0 || usersForm.getPassword().length() == 0) {
+            // 아이디를 입력하지 않았거나 비밀번호를 입력하지 않은 경우
+            return false;
+        }
+        else if (usersRepository.findById(usersForm.getUserId()).isEmpty()){
+            // 아이디를 비교했을 때, 저장되지 않은 아이디일 경우
+            return false;
+        }
+        else if (usersRepository.findByIdAndPassword(usersForm.getPassword(), usersForm.getUserId()).isEmpty()){
+            // 아이디와 패스워드를 비교했을 때, 짝이 맞지 않는 경우
+            return false;
+        }
+        else{
+            httpSession.setAttribute("UsersId", usersForm.getUserId());
+            httpSession.setAttribute("UsersPassword", usersForm.getPassword());
+
+            return true;
+        }
     }
 }
