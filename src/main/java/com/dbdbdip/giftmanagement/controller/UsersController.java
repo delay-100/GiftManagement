dce755963559e2c0b9b06cec16e84bb3d86b241f
sseen2 <59137639+sseen2@users.noreply.github.com>
2023-06-02 01:14:45 +0900

    STYLE: 로그인 성공 시 이동 경로 수정 및 html 이름 변경

--- src/main/java/com/dbdbdip/giftmanagement/controller/UsersController.java
+++ src/main/java/com/dbdbdip/giftmanagement/controller/UsersController.java
@@ -21,7 +21,7 @@ public class UsersController {
 
     @GetMapping("/signup")
     public String createSignupForm() {
-        return "users/RegisterUsersForm";
+        return "users/registerUsersForm";
     }
 
     @PostMapping("/signup")
@@ -29,25 +29,25 @@ public class UsersController {
         System.out.println("******** 회원가입 폼 시작 ********");
         if (usersForm.getUserId().length() == 0) {
             mav.addObject("data", new Message("ID를 입력해주세요.", "signup"));
-            mav.setViewName("/common/Message");
+            mav.setViewName("/common/message");
         }
         else if (usersForm.getPassword().length() == 0) {
             mav.addObject("data", new Message("비밀번호를 입력해주세요.", "signup"));
-            mav.setViewName("/common/Message");
+            mav.setViewName("/common/message");
         }
         else if (usersForm.getNickname().length() == 0) {
             mav.addObject("data", new Message("닉네임을 입력해주세요.", "signup"));
-            mav.setViewName("/common/Message");
+            mav.setViewName("/common/message");
         }
         else if (usersForm.getUserRole() == null) {
             mav.addObject("data", new Message("역할을 선택해주세요.", "signup"));
-            mav.setViewName("/common/Message");
+            mav.setViewName("/common/message");
         }
         else {
             usersService.join(usersForm);
 
             mav.addObject("data", new Message("회원가입이 완료되었습니다.", "login"));
-            mav.setViewName("/common/Message");
+            mav.setViewName("/common/message");
         }
 
         return mav;
@@ -55,7 +55,7 @@ public class UsersController {
 
     @GetMapping("/login")
     public String createLoginForm() {
-        return "users/LoginUsersForm";
+        return "users/loginUsersForm";
     }
 
     @PostMapping("/login")
@@ -64,12 +64,12 @@ public class UsersController {
         System.out.println(usersForm.getUserId() + " " + usersForm.getPassword());
         boolean isSuccess = usersService.login(usersForm, httpSession);
         if (isSuccess) {
-            mav.addObject("data", new Message("로그인 성공.", "login"));
-            mav.setViewName("/common/Message");
+            mav.addObject("data", new Message("로그인 성공.", "../gift"));
+            mav.setViewName("/common/message");
         }
         else {
             mav.addObject("data", new Message("ID나 비밀번호를 확인해주세요.", "login"));
-            mav.setViewName("/common/Message");
+            mav.setViewName("/common/message");
         }
         return mav;
     }
